fba63e8aaaefc7a98749fde99757fe845e762352
Mike <migueldiganchi@gmail.com>
2023-10-15 06:54:02 -0300

    Tips added

--- data/tips-data.js
+++ data/tips-data.js
@@ -1,61 +1,433 @@
 export default [
   {
-    id: 2,
-    title: "Opera con dinero que no necesites",
+    id: 1,
+    title: "Gestiona tus emociones",
     description: `
       <p class="mb-3">
-        Parece una obviedad, pero no lo es, el Trading es un trabajo donde puedes perder todo el capital invertido.
+        Mantén la calma en momentos de volatilidad. Emociones pueden llevar a decisiones impulsivas.
       </p>
       <p>
-        Depositar en la cuenta de Trading tan solo el dinero que no necesitas es una manera de asegurarte de que nunca perderás más dinero del que puedas permitirte.
+        Un trader disciplinado toma decisiones basadas en estrategia, no en emociones.
+      </p>
+    `,
+  },
+  {
+    id: 2,
+    title: "Diversifica tu cartera",
+    description: `
+      <p class="mb-3">
+        No coloques todos tus huevos en una sola canasta. Distribuir riesgos es clave para la estabilidad.
+      </p>
+      <p>
+        Diversificar entre activos y sectores puede ayudarte a mitigar pérdidas.
+      </p>
+    `,
+  },
+  {
+    id: 3,
+    title: "Planifica tus operaciones",
+    description: `
+      <p class="mb-3">
+        Cada operación debe basarse en un plan sólido. Define tus objetivos, entrada y salida antes de operar.
+      </p>
+      <p>
+        El trading sin un plan es como navegar sin brújula.
       </p>
     `,
   },
   {
     id: 4,
-    title: "Utiliza el simulador",
+    title: "Mantén un diario de trading",
     description: `
       <p class="mb-3">
-        El simulador es la herramienta fundamental para empezar a hacer Trading, nos permite probar estrategias sin arriesgar dinero.
+        Registra cada operación, tus pensamientos y emociones. Esto te ayuda a aprender de tus errores y éxitos.
       </p>
       <p>
-        Una vez que veamos que utilizando el simulador somos rentables, ya podremos pasar a lo real. Aunque debes tener en cuenta que en la realidad, los resultados siempre serán peores que con el simulador.
+        El autoanálisis es fundamental para el crecimiento como trader.
+      </p>
+    `,
+  },
+  {
+    id: 5,
+    title: "Establece límites de pérdida",
+    description: `
+      <p class="mb-3">
+        Define cuánto estás dispuesto a perder en una operación. No persigas pérdidas, corta tus pérdidas a tiempo.
+      </p>
+      <p>
+        La gestión de riesgos es esencial para la supervivencia a largo plazo.
       </p>
     `,
   },
   {
     id: 6,
-    title: "No operes en la apertura USA",
+    title: "Analiza las tendencias",
     description: `
       <p class="mb-3">
-        La apertura USA es el momento de mayor volatilidad, todos los peces grandes están posicionándose y eso genera velas muy grandes con muy altos volúmenes. Los stops están demasiado lejos, por lo que el riesgo es, generalmente demasiado elevado
+        Comprende la dirección general del mercado. Operar con la tendencia puede ser más seguro.
+      </p>
+      <p>
+        El viejo dicho "la tendencia es tu amiga" sigue siendo relevante.
+      </p>
+    `,
+  },
+  {
+    id: 7,
+    title: "No sigas a la multitud",
+    description: `
+      <p class="mb-3">
+        La mayoría no siempre tiene razón. Hacer lo contrario de la multitud puede ser beneficioso.
+      </p>
+      <p>
+        El consenso no garantiza el éxito en el trading.
       </p>
     `,
   },
   {
     id: 8,
-    title: "No operes Noticias",
+    title: "Aprende de tus errores",
     description: `
       <p class="mb-3">
-        La noticia no es más que la excusa para mover el precio en l adirección que quieren los profesionales, nunca operes en base al resultado de la noticia
+        Las pérdidas son lecciones. Analiza tus operaciones perdedoras para evitar cometer los mismos errores.
+      </p>
+      <p>
+        El trading es un proceso de mejora continua.
+      </p>
+    `,
+  },
+  {
+    id: 9,
+    title: "Controla tu apalancamiento",
+    description: `
+      <p class="mb-3">
+        El apalancamiento amplifica ganancias y pérdidas. Úsalo con precaución.
+      </p>
+      <p>
+        Un alto apalancamiento puede llevar a desastres financieros.
       </p>
     `,
   },
   {
     id: 10,
-    title: "Si estas perdiendo, apaga el ordenador",
+    title: "Mantén actualizado",
     description: `
       <p class="mb-3">
-        Cuando tradeamos con el miedo, o las ganas de venganza en la mente, lo hacemos mal
+        Los mercados cambian. Aprende constantemente y adapta tus estrategias a las condiciones actuales.
+      </p>
+      <p>
+        La adaptabilidad es una cualidad valiosa en el trading.
+      </p>
+    `,
+  },
+  {
+    id: 11,
+    title: "Sigue un horario consistente",
+    description: `
+      <p class="mb-3">
+        Establece un horario de trading y sé consistente. La disciplina es clave para el éxito.
+      </p>
+      <p>
+        Evita operar impulsivamente fuera de tu horario establecido.
       </p>
     `,
   },
   {
     id: 12,
-    title: "No por tener más indicadores operas mejor",
+    title: "No sigas señales ciegamente",
     description: `
       <p class="mb-3">
-      Los indicadores nos pueden ayudar a tomar decisiones, pero en excel hará que esperemos demasiadas confirmaciones.
+        Realiza tu propia investigación y análisis. No dependas por completo de las señales de otros.
+      </p>
+      <p>
+        La independencia en el trading puede llevarte lejos.
+      </p>
+    `,
+  },
+  {
+    id: 13,
+    title: "Aprovecha la gestión de riesgos",
+    description: `
+      <p class="mb-3">
+        Limita el tamaño de tus posiciones y utiliza stops. Protege tu capital a toda costa.
+      </p>
+      <p>
+        La gestión de riesgos es la clave para la supervivencia a largo plazo.
+      </p>
+    `,
+  },
+  {
+    id: 14,
+    title: "Evita el exceso de confianza",
+    description: `
+      <p class="mb-3">
+        La sobreconfianza puede llevar a decisiones erróneas. Mantén humildad en tus éxitos.
+      </p>
+      <p>
+        El mercado siempre puede sorprender.
+      </p>
+    `,
+  },
+  {
+    id: 15,
+    title: "Adapta tu estrategia",
+    description: `
+      <p class="mb-3">
+        No todas las estrategias funcionan en todos los mercados. Adapta tu enfoque según la situación.
+      </p>
+      <p>
+        Flexibilidad es una ventaja en el trading.
+      </p>
+    `,
+  },
+  {
+    id: 16,
+    title: "Mantén la paciencia",
+    description: `
+      <p class="mb-3">
+        Las prisas pueden llevar a operaciones impulsivas. Espera el momento adecuado para entrar.
+      </p>
+      <p>
+        La paciencia es una virtud en el trading.
+      </p>
+    `,
+  },
+  {
+    id: 17,
+    title: "Evita el sobreanálisis",
+    description: `
+      <p class="mb-3">
+        No te paralices por el análisis excesivo. A veces, una decisión rápida es mejor que ninguna.
+      </p>
+      <p>
+        El equilibrio entre análisis y acción es esencial.
+      </p>
+    `,
+  },
+  {
+    id: 18,
+    title: "Define tus metas",
+    description: `
+      <p class="mb-3">
+        Ten claro lo que deseas lograr en el trading. Establece metas realistas y alcanzables.
+      </p>
+      <p>
+        Las metas te dan dirección y propósito.
+      </p>
+    `,
+  },
+  {
+    id: 19,
+    title: "Mantén un estilo de vida saludable",
+    description: `
+      <p class="mb-3">
+        El bienestar físico y mental influye en tu desempeño. Descansa, ejercítate y come bien.
+      </p>
+      <p>
+        Un trader saludable es un trader más eficiente.
+      </p>
+    `,
+  },
+  {
+    id: 20,
+    title: "No persigas pérdidas",
+    description: `
+      <p class="mb-3">
+        Si una operación va en contra, no trates de recuperar las pérdidas de inmediato. Mantén la calma.
+      </p>
+      <p>
+        El trading vengativo puede ser costoso.
+      </p>
+    `,
+  },
+  {
+    id: 21,
+    title: "Sé realista",
+    description: `
+      <p class="mb-3">
+        No esperes ganancias enormes de inmediato. El trading es un proceso que lleva tiempo.
+      </p>
+      <p>
+        La consistencia a largo plazo es más valiosa que las ganancias fugaces.
+      </p>
+    `,
+  },
+  {
+    id: 22,
+    title: "Aprende de los profesionales",
+    description: `
+      <p class="mb-3">
+        Estudia el enfoque de traders exitosos. Puedes aprender mucho de su experiencia y estrategias.
+      </p>
+      <p>
+        La educación continua es esencial en el trading.
+      </p>
+    `,
+  },
+  {
+    id: 23,
+    title: "No subestimes los costos",
+    description: `
+      <p class="mb-3">
+        Considera comisiones y spreads al calcular tus ganancias y pérdidas. Pequeños costos pueden sumar.
+      </p>
+      <p>
+        La gestión de costos es parte de la gestión de riesgos.
+      </p>
+    `,
+  },
+  {
+    id: 24,
+    title: "Sigue la actualidad económica",
+    description: `
+      <p class="mb-3">
+        Mantente informado sobre eventos económicos. Las noticias pueden influir en los mercados.
+      </p>
+      <p>
+        El conocimiento es poder en el trading.
+      </p>
+    `,
+  },
+  {
+    id: 25,
+    title: "No seas codicioso",
+    description: `
+      <p class="mb-3">
+        La codicia puede llevar a tomar riesgos excesivos. Define tus objetivos y respétalos.
+      </p>
+      <p>
+        La avaricia puede ser perjudicial en el trading.
+      </p>
+    `,
+  },
+  {
+    id: 26,
+    title: "Acepta pérdidas como parte del juego",
+    description: `
+      <p class="mb-3">
+        Pérdidas son inevitables. No dejes que te desanimen. Aprende y sigue adelante.
+      </p>
+      <p>
+        El trading es una maratón, no una carrera.
+      </p>
+    `,
+  },
+  {
+    id: 27,
+    title: "Mantén contacto con otros traders",
+    description: `
+      <p class="mb-3">
+        Comparte experiencias con otros traders. La comunidad puede ofrecer apoyo y consejos valiosos.
+      </p>
+      <p>
+        No estás solo en este viaje.
+      </p>
+    `,
+  },
+  {
+    id: 28,
+    title: "Observa la liquidez",
+    description: `
+      <p class="mb-3">
+        Asegúrate de que estás operando activos líquidos. La liquidez facilita la entrada y salida de posiciones.
+      </p>
+      <p>
+        Evita activos con poca liquidez.
+      </p>
+    `,
+  },
+  {
+    id: 29,
+    title: "Sé consistente en tu enfoque",
+    description: `
+      <p class="mb-3">
+        Cambiar constantemente de estrategia puede ser perjudicial. Mantén un enfoque coherente.
+      </p>
+      <p>
+        La consistencia puede conducir a la mejora continua.
+      </p>
+    `,
+  },
+  {
+    id: 30,
+    title: "Evita el ruido del mercado",
+    description: `
+      <p class="mb-3">
+        No te dejes llevar por el ruido del mercado. Escucha tu propio análisis y estrategia.
+      </p>
+      <p>
+        La confusión puede llevar a malas decisiones.
+      </p>
+    `,
+  },
+  {
+    id: 31,
+    title: "Sigue la tendencia, no adivines contra ella",
+    description: `
+      <p class="mb-3">
+        Operar contra la tendencia es arriesgado. Sigue la dirección general del mercado.
+      </p>
+      <p>
+        El mercado tiene tendencia a seguir tendencias.
+      </p>
+    `,
+  },
+  {
+    id: 32,
+    title: "Mide tu desempeño",
+    description: `
+      <p class="mb-3">
+        Lleva un registro de tus operaciones y resultados. Esto te permite evaluar tu desempeño a lo largo del tiempo.
+      </p>
+      <p>
+        La mejora requiere medición.
+      </p>
+    `,
+  },
+  {
+    id: 33,
+    title: "No te excedas en el tamaño de posición",
+    description: `
+      <p class="mb-3">
+        Un tamaño de posición demasiado grande puede ser arriesgado. Define un tamaño adecuado según tu capital.
+      </p>
+      <p>
+        La gestión de tamaño es esencial.
+      </p>
+    `,
+  },
+  {
+    id: 34,
+    title: "Aprende de los errores de otros",
+    description: `
+      <p class="mb-3">
+        Estudia los errores comunes de otros traders. Puedes evitar cometer los mismos errores.
+      </p>
+      <p>
+        La educación a través de la experiencia ajena es valiosa.
+      </p>
+    `,
+  },
+  {
+    id: 35,
+    title: "Sé realista en tus expectativas",
+    description: `
+      <p class="mb-3">
+        No esperes ganancias descomunales. El trading requiere tiempo y paciencia.
+      </p>
+      <p>
+        El realismo te mantendrá centrado en metas alcanzables.
+      </p>
+    `,
+  },
+  {
+    id: 36,
+    title: "Confía en tu análisis",
+    description: `
+      <p class="mb-3">
+        Cuando hagas un análisis sólido, ten confianza en él. No te dejes influenciar fácilmente por el mercado.
+      </p>
+      <p>
+        La autoconfianza es un activo valioso en el trading.
       </p>
     `,
   },
